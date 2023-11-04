e5f74a83c82549488a8f1eaef2a0fc5bc4bd9f4d
Tyler Nguyen <tylernn812@gmail.com>
2023-11-04 16:00:02 -0700

    initial commit

---
+++ RetirementCalculator/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  RetirementCalculator
+//
+//  Created by Tyler on 2023-11-04.
+//
+
+import UIKit
+import AppCenter
+import AppCenterCrashes
+import AppCenterAnalytics
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        AppCenter.start(withAppSecret: "bc651117-2f1c-412e-ac53-7b051936e308", services: [
+            Analytics.self,
+            Crashes.self
+        ])
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
